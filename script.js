f45593a8450735998baba7f475f24d3b8f483fb8
vnd255 <dungnvbk255@gmail.com>
2022-08-24 20:06:16 +0700

    haha

--- script.js
+++ script.js
@@ -24,17 +24,19 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 const saveImage = () => {
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
-    canvas.width = 960;
-    canvas.height = 1280;
+    canvas.width = previewImg.naturalWidth;
+    canvas.height = previewImg.naturalHeight;
+    // canvas.width = 960;
+    // canvas.height = 1280;
     const x = (canvas.width / 2) - 3.5;
     const y = (-canvas.height / 2) + 35;
     canvas.style.letterSpacing = '1px';
     ctx.translate(canvas.width / 2, canvas.height / 2);
     ctx.drawImage(previewImg, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
-    ctx.font = "20pt Verdana";
+    ctx.font = "19pt Verdana";
     ctx.fillStyle = "white";
     ctx.textAlign = "end";
-    ctx.fillText('Network: 22 thg 8, 2022 20:00:45 GMT+07:00',x,y);
+    ctx.fillText('Network: 22 thg8, 2022 20:00:45 GMT+07:00',x,y);
     ctx.font = "20pt Verdana";
     ctx.fillText('Việt Yên',x,y + 35);
     ctx.fillText('Bắc Giang',x,y + 74);
