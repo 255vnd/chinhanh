3795ffa0435610f30aafb11124b55ae53eb74caf
vnd255 <dungnvbk255@gmail.com>
2022-08-24 20:33:31 +0700

    haha

--- script.js
+++ script.js
@@ -28,16 +28,27 @@ const saveImage = () => {
     // canvas.height = previewImg.naturalHeight;
     canvas.width = 960;
     canvas.height = 1280;
+
     const x = (canvas.width / 2) - 3.5;
     const y = (-canvas.height / 2) + 35;
-    canvas.style.letterSpacing = '1px';
+
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1;
+    let yyyy = today.getFullYear();
+    let h = 20;
+    let m = String(Math.floor(Math.random() * 3)).padStart(2, '0');
+    let s = String(Math.floor(Math.random() * 60)).padStart(2, '0');
+
+    const text = `Network: ${dd} thg ${mm}, ${yyyy} 20:${m}:${s} GMT+07:00`;
+    
     ctx.translate(canvas.width / 2, canvas.height / 2);
     ctx.drawImage(previewImg, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
-    ctx.font = "22pt Verdana";
+    ctx.font = "19pt Verdana";
     ctx.fillStyle = "white";
     ctx.textAlign = "end";
-    ctx.fillText('Network: 22 thg 8, 2022 20:00:45 GMT+07:00',x,y);
-    ctx.font = "20pt Verdana";
+    ctx.fillText(text,x,y);
+    ctx.font = "19pt Verdana";
     ctx.fillText('Việt Yên',x,y + 35);
     ctx.fillText('Bắc Giang',x,y + 74);
     const link = document.createElement("a");
